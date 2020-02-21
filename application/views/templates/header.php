30e427a4eebcbc09137541a3e61a8cd83c19e370
Justin <jdalcorn01@gmail.com>
2020-02-21 15:43:39 -0500

    Added bootstrap and started configuring custom views

---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Recipe Submission</title>
+		<link href="<php echo base_url(); ?>assets/bootstrap-4.3.1-dist/css/bootstrap.css" rel="stylesheet" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	</head>
+	<body>
